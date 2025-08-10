795dab5479f0ea8cc225b8d70d287514fdef2479
Umut Sahin <umut.sahin@supplychainwizard.com>
2025-08-10 11:07:06 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,98 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import {
+  IInputsFormGroup,
+  NgxMatCronSelectComponent,
+} from 'ngx-mat-cron-select';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NgxMatCronSelectComponent, ReactiveFormsModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  providers: [HttpClient],
 })
 export class AppComponent {
+  public readonly translate = inject(TranslateService);
+  public isDisabled = false;
+  public value = '1 * 3 4 5';
+  public valueChange: string | null = null;
+
   title = 'ngx-mat-cron-select-playground';
+  lalala = this.translate.instant('ngxMatCronSelect.dayOfMonthSelectLabel');
+
+  public inputsFormGroup = new FormGroup<IInputsFormGroup>({
+    dayOfMonth: new FormControl<number[]>([], {
+      nonNullable: true,
+      validators: [Validators.required],
+    }),
+    dayOfWeek: new FormControl<number[]>([], {
+      nonNullable: true,
+      validators: [Validators.required],
+    }),
+    hour: new FormControl<number | null>(null, {
+      validators: [Validators.required],
+    }),
+    // hour: new FormControl<number[]>([], {nonNullable: true, validators: [Validators.required] }),
+    minute: new FormControl<number[]>([], {
+      nonNullable: true,
+      validators: [Validators.required],
+    }),
+    monthOfYear: new FormControl<number[]>([], {
+      nonNullable: true,
+      validators: [Validators.required],
+    }),
+  });
+  public readonly formControl = new FormControl('1 2 3 * 5', [
+    Validators.required,
+  ]);
+
+  public disableSelect(): void {
+    this.isDisabled = !this.isDisabled;
+  }
+
+  public setValue(): void {
+    this.value = '* 2 * * *';
+  }
+
+  public setValueFC(): void {
+    this.formControl.setValue(null);
+  }
+
+  public setFGVal(): void {
+    this.inputsFormGroup.controls.hour.setValue(10);
+  }
+
+  public setFG(): void {
+    this.inputsFormGroup = new FormGroup<IInputsFormGroup>({
+      dayOfMonth: new FormControl<number[]>([], {
+        nonNullable: true,
+        validators: [Validators.required],
+      }),
+      dayOfWeek: new FormControl<number[]>([], {
+        nonNullable: true,
+        validators: [Validators.required],
+      }),
+      hour: new FormControl<number[]>([], {
+        validators: [Validators.required],
+      }),
+      // hour: new FormControl<number[]>([], {nonNullable: true, validators: [Validators.required] }),
+      minute: new FormControl<number[]>([], {
+        nonNullable: true,
+        validators: [Validators.required],
+      }),
+      monthOfYear: new FormControl<number[]>([], {
+        nonNullable: true,
+        validators: [Validators.required],
+      }),
+    });
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,115 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  inject,
+  provideAppInitializer,
+  provideZoneChangeDetection,
+  signal,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import {
+  HttpClient,
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
+import {
+  TranslateLoader,
+  TranslateModule,
+  TranslateService,
+} from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import {
+  MAT_DATE_LOCALE,
+  provideNativeDateAdapter,
+} from '@angular/material/core';
+import {
+  NGX_MAT_CRON_SELECT_IS_TWELVE_HOUR,
+  NGX_MAT_CRON_SELECT_MONTH_FORMAT,
+  NGX_MAT_CRON_SELECT_TRANSLATE_SERVICE,
+  NGX_MAT_CRON_SELECT_WEEK_FORMAT,
+  provideNMCSTranslations,
+} from 'ngx-mat-cron-select';
+import { filter, of, take } from 'rxjs';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(withInterceptorsFromDi()),
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        loader: {
+          deps: [HttpClient],
+          provide: TranslateLoader,
+          useFactory: httpLoaderFactory,
+        },
+      }),
+    ),
+    provideAppInitializer(() =>
+      appInitializerFactory(inject(TranslateService))(),
+    ),
+    provideNativeDateAdapter(),
+    { provide: NGX_MAT_CRON_SELECT_IS_TWELVE_HOUR, useValue: signal(false) },
+    { provide: NGX_MAT_CRON_SELECT_WEEK_FORMAT, useValue: 'long' },
+    { provide: NGX_MAT_CRON_SELECT_MONTH_FORMAT, useValue: 'long' },
+    { provide: MAT_DATE_LOCALE, useValue: 'tr-TR' },
+    provideNMCSTranslations(
+      of({
+        en: {
+          dayOfMonthSelectLabel: 'Select days',
+          dayOfWeekSelectLabel: 'Select days of week',
+          everyDayLabel: 'Every Day',
+          everyHourLabel: 'Every Hour',
+          everyMinuteLabel: 'Every Minute',
+          everyMonthLabel: 'Every Monthh',
+          hourSelectLabel: 'Select hours',
+          minuteSelectLabel: 'Select minutes',
+          monthSelectLabel: 'Select months',
+          tabLabelDay: 'Hour',
+          tabLabelHour: 'Day',
+          tabLabelMonth: 'Month',
+          tabLabelWeek: 'Week',
+          tabLabelYear: 'Year',
+        },
+      }),
+      of('en'),
+    ),
+    // {
+    //   provide: NGX_MAT_CRON_SELECT_TRANSLATE_SERVICE,
+    //   useExisting: TranslateService,
+    // },
+  ],
 };
+
+function httpLoaderFactory(httpClient: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(httpClient, 'assets/i18n/', `.json`);
+}
+
+function appInitializerFactory(
+  translate: TranslateService,
+): () => Promise<void> {
+  return () =>
+    new Promise<void>(
+      (resolve: (value: void) => void, reject: (value: void) => void): void => {
+        translate.setDefaultLang('en');
+        translate
+          .use('en')
+          .pipe(
+            filter(
+              (translateResponseData): boolean =>
+                translateResponseData !== undefined,
+            ),
+            take(1),
+          )
+          .subscribe({
+            complete: (): void => {
+              resolve();
+            },
+            error: (): void => {
+              reject();
+            },
+          });
+      },
+    );
+}
